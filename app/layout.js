9575d9a1c7d16d62e25fb32eb07c73baddec31b5
Nazarene Gena <genarene96@gmail.com>
2023-08-25 12:40:24 +0300

    changed background to only landing page

--- app/layout.js
+++ app/layout.js
@@ -13,10 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${font.className} px-10
-       py-5
-       bg-gradient-to-t from-white to-rose-100 
-       `}>
+      <body className={`${font.className}`}>
         <div className="flex justify-between py-6">
           <p>BeatSocial logo</p>
           <Navbar />
